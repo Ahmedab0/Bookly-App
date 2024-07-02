bc8455556f684e0a57a42668c88c9b00d8c10982
Ahmed Mohamed <ahmedabosena055@gmail.com>
2024-07-02 22:01:14 +0300

    create GetIt setupServiceLocator

--- lib/features/Home_Feature/presentation/Manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/Home_Feature/presentation/Manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,5 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/Home_Feature/data/repos/home_repo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 
 import '../../../data/models/BookModel.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
+import 'package:bookly_app/features/Home_Feature/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/features/Home_Feature/presentation/Manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly_app/features/splash_feature/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'core/utils/service_locator.dart';
+import 'features/Home_Feature/presentation/Manager/featured_books_cubit/featured_books_cubit.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const MyApp());
 }
 
@@ -13,30 +19,42 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (BuildContext context) => FeaturedBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+        BlocProvider(
+          create: (BuildContext context) => NewestBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const SplashView(),
+        onGenerateRoute: AppRouter().switchRoute,
+        /* (settings) {
+          switch (settings.name) {
+            case Home.routeNamed:
+              return PageTransition(
+                  child: const Home(), type: PageTransitionType.leftToRight);
+            //break;
+            case BookDetailsView.routeNamed:
+              return PageTransition(
+                  child: const BookDetailsView(),
+                  type: PageTransitionType.leftToRight);
+            //break;
+            default:
+              return null;
+          }
+        },*/
       ),
-      debugShowCheckedModeBanner: false,
-      home: const SplashView(),
-      onGenerateRoute: AppRouter().switchRoute,
-         /* (settings) {
-        switch (settings.name) {
-          case Home.routeNamed:
-            return PageTransition(
-                child: const Home(), type: PageTransitionType.leftToRight);
-          //break;
-          case BookDetailsView.routeNamed:
-            return PageTransition(
-                child: const BookDetailsView(),
-                type: PageTransitionType.leftToRight);
-          //break;
-          default:
-            return null;
-        }
-      },*/
     );
   }
 }
-
-
